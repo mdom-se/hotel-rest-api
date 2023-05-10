9a76b2be46a5801a9ae79bc1a20210d309e67ffa
Mario Dominguez <mdom.job.uni@gmail.com>
2023-05-10 10:59:10 -0600

    Create the scaffold for the App

---
+++ src/main/java/com/demo/hotelrestapi/ServletInitializer.java
@@ -0,0 +1,16 @@
+package com.demo.hotelrestapi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ServletInitializer.class);
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		LOGGER.info("Starting app HotelRestApiApplication...");
+		return application.sources(HotelRestApiApplication.class);
+	}
+
+}
