c7654f38fe1f9b621ca3aa92e36ddd453dd1eec2
Mario Dominguez <mdom.job.uni@gmail.com>
2023-05-12 17:38:09 -0600

    Add Hotel Webservice client

--- src/main/java/com/demo/hotel/restcontroller/HotelController.java
+++ src/main/java/com/demo/hotel/restcontroller/HotelController.java
@@ -1,14 +1,17 @@
-package com.demo.hotelrestapi.restcontroller;
+package com.demo.hotel.restcontroller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PingController {
+@RequestMapping("/hotel")
+public class HotelController {
 
     @GetMapping("/ping")
     public ResponseEntity<String> ping(){
+
         return ResponseEntity.ok("ping");
     }
 }
--- src/test/java/com/demo/hotel/HotelRestApiApplicationTests.java
+++ src/test/java/com/demo/hotel/HotelRestApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.demo.hotelrestapi;
+package com.demo.hotel;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
